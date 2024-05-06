09346026d82a84d991b7eba9e747c8202f126574
zlhe <heziliorg@163.com>
2024-05-06 18:58:21 +0800

    Update run.sh

--- src/01/run.sh
+++ src/01/run.sh
@@ -6,5 +6,8 @@ main_nvcc="nvcc --compiler-bindir /usr/bin/g++-10"
 
 target="saxpy"
 $main_nvcc $target.cu -o out/$target
-echo "compiled successfully!!!"
+echo "[ Cuda] compiled successfully!!!"
 out/$target
+
+gcc saxpy.c -o out/saxpy_c
+out/saxpy_c
